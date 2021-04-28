3a1d89f105050ac77c928d5fbc8f589249173fe6
Lauro de Paula Gomes <laurobmb@gmail.com>
2021-04-28 18:50:16 -0300

    Atualizando regras

--- scripts/delete_rule.py
+++ scripts/delete_rule.py
@@ -4,12 +4,7 @@ import os
 path='/etc/nginx/modsecurity/rules/'
 
 files=[
-    path+'REQUEST-903.9006-XENFORO-EXCLUSION-RULES.conf',
-    path+'REQUEST-903.9005-CPANEL-EXCLUSION-RULES.conf',
-    path+'REQUEST-903.9004-DOKUWIKI-EXCLUSION-RULES.conf',
-    path+'REQUEST-903.9003-NEXTCLOUD-EXCLUSION-RULES.conf',
-    path+'REQUEST-903.9002-WORDPRESS-EXCLUSION-RULES.conf',
-    path+'REQUEST-903.9001-DRUPAL-EXCLUSION-RULES.conf'
+    path+'REQUEST-903.9006-XENFORO-EXCLUSION-RULES.conf'
 ]
 
 print("########## Apagando rules do CRS ##########")
--- scripts/disable_rule.py
+++ scripts/disable_rule.py
@@ -5,9 +5,7 @@ print("########## File is /etc/nginx/modsecurity/crs-setup.conf ##########")
 
 file='/etc/nginx/modsecurity/crs-setup.conf'
 
-rules=[954011,954012,954100,954110,954120,954130,          # regras foram desativadas por não fazerem sentido
-        954013,954014,954015,954016,954017,954018,920350,  # regras foram desativadas por não fazerem sentido
-        911100,930120,942100]  # regras foram desativadas por adaptação
+rules=[954011]
 
 with open(file, "a") as file_object:
     file_object.write('### Editado ao iniciar #### \n')
